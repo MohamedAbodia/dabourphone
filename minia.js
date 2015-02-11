3abcee0a26a3b22bdc4f907185ffe2421d7f44cd
moda01234567889 <moda01234567889@gmail.com>
2015-02-11 17:50:56 +0000

    minia

---
+++ minia.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+//<![CDATA[
+window.onload = function(){
+
+    var copy=document.getElementById("dabourphone");
+    
+    if(copy==null){
+        window.location.href="http://dabourphone.blogspot.com/";
+    };
+    copy.setAttribute("href","http://dabourphone.blogspot.com/");
+    copy.innerHTML="ÏÈæÑ Ýæä"
+};
+//]]>
+</script>
